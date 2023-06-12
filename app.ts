88726977f690a32b178e0384f3bf101a9c61092f
Suraj <surajchan68@gmail.com>
2023-06-12 22:41:16 +0545

    feat: add folder structure up to modules

--- app.ts
+++ app.ts
@@ -1,9 +1,35 @@
-import express, { Request, Response, NextFunction, Application } from "express";
-
+import express, { Application, NextFunction, Request, Response } from "express";
 const app: Application = express();
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import appRoute from "src/routes";
+import path from "path";
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Server is ready");
-});
+try {
+  app.use(cors());
+  app.use(morgan("dev"));
+  app.set("rateLimit", 100);
+
+  app.options("*", cors());
+  app.use(helmet());
+  app.use("/public", express.static(path.join(__dirname, "public")));
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  app.use((req: Request, res: Response, next: NextFunction): void => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Authorization,x-access-token,Accept,Origin");
+    res.setHeader("Cache-Control", 'no-cache="Set-Cookie, Set-Cookie2"');
+    next();
+  });
+
+  appRoute(app);
+} catch (err) {
+  console.log("error", err);
+  process.exit(0);
+}
 
 export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import { Application } from "express";
+
+const appRoute = (router: Application): void => {
+  //   app.use("");
+};
+
+export default appRoute;

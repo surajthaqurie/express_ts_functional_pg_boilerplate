c1f79f025264f0a9a69375a679555a17cac50860
bikesh <sitikhubikesh@gmail.com>
2023-06-13 23:34:17 +0545

    chore: remove unnecessary comments

--- app.ts
+++ app.ts
@@ -2,41 +2,9 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
-// import appRoute from "src/routes";
 import path from "path";
 
 import indexRouter from "./src/routes";
-
-// try {
-//   app.use(cors());
-//   app.use(morgan("dev"));
-//   app.set("rateLimit", 100);
-
-//   app.options("*", cors());
-//   app.use(helmet());
-//   app.use("/public", express.static(path.join(__dirname, "public")));
-
-//   app.use(express.json());
-//   app.use(express.urlencoded({ extended: false }));
-
-//   app.use((req: Request, res: Response, next: NextFunction): void => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
-//     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Authorization,x-access-token,Accept,Origin");
-//     res.setHeader("Cache-Control", 'no-cache="Set-Cookie, Set-Cookie2"');
-//     next();
-//   });
-
-//   appRoute(app);
-
-//   // todo: not found route
-//   // todo: error handling
-// } catch (err) {
-//   console.log("error", err);
-//   process.exit(0);
-// }
-
-// export default app;
 
 class App {
   public app: express.Application;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,3 @@
-// import { Application } from "express";
-
-// const appRoute = (router: Application): void => {
-//   //   app.use("");
-// };
-
-// export default appRoute;
-
-// // todo: separate app user role routes
-
 import { Router } from "express";
 import authRouter from "../modules/auth/auth.route";
 
